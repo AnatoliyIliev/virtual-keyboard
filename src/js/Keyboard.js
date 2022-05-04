39b01a8ca889d23ec86d55d143e97f9dd420d2e0
AnatoliyIliev <vekok12@gmail.com>
2022-05-04 19:02:51 +0300

    init: createKeys

--- src/js/Keyboard.js
+++ src/js/Keyboard.js
@@ -1,4 +1,4 @@
-// import keys from './keys.js';
+import keyLayout from './keys.js';
 
 export default class Keyboard {
   constructor() {
@@ -11,34 +11,31 @@ export default class Keyboard {
     this.capsLock = false;
   }
 
-  //   elements() {
-  //     this.main = null;
-  //     this.keysContainer = null;
-  //     this.keys = [];
-  //   }
-
-  //   eventHandlers() {
-  //     this.onInput = null;
-  //     this.onClose = null;
-  //   }
-
-  //   properties() {
-  //     this.value = '';
-  //     this.capsLock = false;
-  //   }
-
   init() {
-    this.elements.main = document.createElement('div');
-    this.elements.keysContainer = document.createElement('div');
+    this.main = document.createElement('div');
+    this.keysContainer = document.createElement('div');
 
-    this.elements.main.classList.add('keyboard', 'keyboard-hidden');
-    this.elements.keysContainer.classList.add('keyboard__keys');
+    this.main.classList.add('keyboard', 'keyboard--hidden');
+    this.keysContainer.classList.add('keyboard__keys');
 
-    this.elements.main.appendChild(this.elements.keysContainer);
+    this.main.appendChild(this.elements.keysContainer);
     document.body.appendChild(this.elements.main);
   }
 
   _createKeys() {
     const fragment = document.createDocumentFragment();
+
+    const createIcon = iconName => {
+      return `<i class="icons">${iconName}</i>`;
+    };
+
+    keyLayout.forEach(key => {
+      const keyElement = document.createElement('button');
+      const insertLineBreack =
+        ['backspace', 'del', 'enter', 'shift', 'ctrl'].indexOf(key) !== -1;
+
+      keyElement.setAttribute('type', 'button');
+      keyElement.classList.add('keyboard__key');
+    });
   }
 }
