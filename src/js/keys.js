1ba818d59101014475db917124557194c260aef6
AnatoliyIliev <vekok12@gmail.com>
2022-05-08 12:48:53 +0300

    fix: _createKeys render keys

--- src/js/keys.js
+++ src/js/keys.js
@@ -1,81 +1,422 @@
-export default {
-  Backquote: {
+export default [
+  {
+    keyCode: 'Backquote',
     en: { normal: '`', shift: '~' },
     ru: { normal: 'ё', shift: 'Ё' },
+    universalButton: false,
   },
-  Digit1: { en: { normal: '1', shift: '!' }, ru: { normal: '1', shift: '!' } },
-  Digit2: { en: { normal: '2', shift: '@' }, ru: { normal: '2', shift: '"' } },
-  Digit3: { en: { normal: '3', shift: '#' }, ru: { normal: '3', shift: '№' } },
-  Digit4: { en: { normal: '4', shift: '$' }, ru: { normal: '4', shift: ';' } },
-  Digit5: { en: { normal: '5', shift: '%' }, ru: { normal: '5', shift: '%' } },
-  Digit6: { en: { normal: '6', shift: '^' }, ru: { normal: '6', shift: ':' } },
-  Digit7: { en: { normal: '7', shift: '&' }, ru: { normal: '7', shift: '?' } },
-  Digit8: { en: { normal: '8', shift: '*' }, ru: { normal: '8', shift: '*' } },
-  Digit9: { en: { normal: '9', shift: '(' }, ru: { normal: '9', shift: '(' } },
-  Digit0: { en: { normal: '0', shift: ')' }, ru: { normal: '0', shift: ')' } },
-  Minus: { en: { normal: '-', shift: '_' }, ru: { normal: '-', shift: '_' } },
-  Equal: { en: { normal: '=', shift: '+' }, ru: { normal: '=', shift: '+' } },
-  Backspace: 'Backspace',
-  Tab: 'Tab',
-  KeyQ: { en: { normal: 'q', shift: 'Q' }, ru: { normal: 'й', shift: 'Й' } },
-  KeyW: { en: { normal: 'w', shift: 'W' }, ru: { normal: 'ц', shift: 'Ц' } },
-  KeyE: { en: { normal: 'e', shift: 'E' }, ru: { normal: 'у', shift: 'У' } },
-  KeyR: { en: { normal: 'r', shift: 'R' }, ru: { normal: 'к', shift: 'К' } },
-  KeyT: { en: { normal: 't', shift: 'T' }, ru: { normal: 'е', shift: 'Е' } },
-  KeyY: { en: { normal: 'y', shift: 'Y' }, ru: { normal: 'н', shift: 'Н' } },
-  KeyU: { en: { normal: 'u', shift: 'U' }, ru: { normal: 'г', shift: 'Г' } },
-  KeyI: { en: { normal: 'i', shift: 'I' }, ru: { normal: 'ш', shift: 'Ш' } },
-  KeyO: { en: { normal: 'o', shift: 'O' }, ru: { normal: 'щ', shift: 'Щ' } },
-  KeyP: { en: { normal: 'p', shift: 'P' }, ru: { normal: 'з', shift: 'З' } },
-  BracketLeft: {
+  {
+    keyCode: 'Digit1',
+    en: { normal: '1', shift: '!' },
+    ru: { normal: '1', shift: '!' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Digit2',
+    en: { normal: '2', shift: '@' },
+    ru: { normal: '2', shift: '"' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Digit3',
+    en: { normal: '3', shift: '#' },
+    ru: { normal: '3', shift: '№' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Digit4',
+    en: { normal: '4', shift: '$' },
+    ru: { normal: '4', shift: ';' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Digit5',
+    en: { normal: '5', shift: '%' },
+    ru: { normal: '5', shift: '%' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Digit6',
+    en: { normal: '6', shift: '^' },
+    ru: { normal: '6', shift: ':' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Digit7',
+    en: { normal: '7', shift: '&' },
+    ru: { normal: '7', shift: '?' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Digit8',
+    en: { normal: '8', shift: '*' },
+    ru: { normal: '8', shift: '*' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Digit9',
+    en: { normal: '9', shift: '(' },
+    ru: { normal: '9', shift: '(' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Digit0',
+    en: { normal: '0', shift: ')' },
+    ru: { normal: '0', shift: ')' },
+    universalButton: false,
+  },
+  {
+    keyCode: 'Minus',
+    en: { normal: '-', shift: '_' },
+    ru: { normal: '-', shift: '_' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'Equal',
+    en: { normal: '=', shift: '+' },
+    ru: { normal: '=', shift: '+' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'Backspace',
+    en: { normal: 'Backspace' },
+    ru: { normal: 'Backspace' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'Tab',
+    en: { normal: 'Tab' },
+    ru: { normal: 'Tab' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'KeyQ',
+    en: { normal: 'q', shift: 'Q' },
+    ru: { normal: 'й', shift: 'Й' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyW',
+    en: { normal: 'w', shift: 'W' },
+    ru: { normal: 'ц', shift: 'Ц' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyE',
+    en: { normal: 'e', shift: 'E' },
+    ru: { normal: 'у', shift: 'У' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyR',
+    en: { normal: 'r', shift: 'R' },
+    ru: { normal: 'к', shift: 'К' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyT',
+    en: { normal: 't', shift: 'T' },
+    ru: { normal: 'е', shift: 'Е' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyY',
+    en: { normal: 'y', shift: 'Y' },
+    ru: { normal: 'н', shift: 'Н' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyU',
+    en: { normal: 'u', shift: 'U' },
+    ru: { normal: 'г', shift: 'Г' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyI',
+    en: { normal: 'i', shift: 'I' },
+    ru: { normal: 'ш', shift: 'Ш' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyO',
+    en: { normal: 'o', shift: 'O' },
+    ru: { normal: 'щ', shift: 'Щ' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyP',
+    en: { normal: 'p', shift: 'P' },
+    ru: { normal: 'з', shift: 'З' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'BracketLeft',
     en: { normal: '[', shift: '}' },
     ru: { normal: 'х', shift: 'Х' },
+    universalButton: false,
   },
-  BracketRight: {
+
+  {
+    keyCode: 'BracketRight',
     en: { normal: ']', shift: '}' },
     ru: { normal: 'ъ', shift: 'Ъ' },
+    universalButton: false,
   },
-  Backslash: {
+
+  {
+    keyCode: 'Backslash',
     en: { normal: '\\', shift: '|' },
     ru: { normal: '\\', shift: '/' },
+    universalButton: false,
   },
-  Delete: 'Del',
-  CapsLock: 'Caps Lock',
-  KeyA: { en: { normal: 'a', shift: 'A' }, ru: { normal: 'ф', shift: 'Ф' } },
-  KeyS: { en: { normal: 's', shift: 'S' }, ru: { normal: 'ы', shift: 'Ы' } },
-  KeyD: { en: { normal: 'd', shift: 'D' }, ru: { normal: 'в', shift: 'В' } },
-  KeyF: { en: { normal: 'f', shift: 'F' }, ru: { normal: 'а', shift: 'А' } },
-  KeyG: { en: { normal: 'g', shift: 'G' }, ru: { normal: 'п', shift: 'П' } },
-  KeyH: { en: { normal: 'h', shift: 'H' }, ru: { normal: 'р', shift: 'Р' } },
-  KeyJ: { en: { normal: 'j', shift: 'J' }, ru: { normal: 'о', shift: 'О' } },
-  KeyK: { en: { normal: 'k', shift: 'K' }, ru: { normal: 'л', shift: 'Л' } },
-  KeyL: { en: { normal: 'l', shift: 'L' }, ru: { normal: 'д', shift: 'Д' } },
-  Semicolon: {
+
+  {
+    keyCode: 'Delete',
+    en: { normal: 'Del' },
+    ru: { normal: 'Del' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'CapsLock',
+    en: { normal: 'CapsLock' },
+    ru: { normal: 'CapsLock' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'KeyA',
+    en: { normal: 'a', shift: 'A' },
+    ru: { normal: 'ф', shift: 'Ф' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyS',
+    en: { normal: 's', shift: 'S' },
+    ru: { normal: 'ы', shift: 'Ы' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyD',
+    en: { normal: 'd', shift: 'D' },
+    ru: { normal: 'в', shift: 'В' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyF',
+    en: { normal: 'f', shift: 'F' },
+    ru: { normal: 'а', shift: 'А' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyG',
+    en: { normal: 'g', shift: 'G' },
+    ru: { normal: 'п', shift: 'П' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyH',
+    en: { normal: 'h', shift: 'H' },
+    ru: { normal: 'р', shift: 'Р' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyJ',
+    en: { normal: 'j', shift: 'J' },
+    ru: { normal: 'о', shift: 'О' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyK',
+    en: { normal: 'k', shift: 'K' },
+    ru: { normal: 'л', shift: 'Л' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyL',
+    en: { normal: 'l', shift: 'L' },
+    ru: { normal: 'д', shift: 'Д' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'Semicolon',
     en: { normal: ';', shift: ':' },
     ru: { normal: 'ж', shift: 'Ж' },
+    universalButton: false,
   },
-  Quote: { en: { normal: "'", shift: '"' }, ru: { normal: 'э', shift: 'Э' } },
-  Enter: 'Enter',
-  ShiftLeft: 'Shift',
-  KeyZ: { en: { normal: 'z', shift: 'Z' }, ru: { normal: 'я', shift: 'Я' } },
-  KeyX: { en: { normal: 'x', shift: 'X' }, ru: { normal: 'ч', shift: 'Ч' } },
-  KeyC: { en: { normal: 'c', shift: 'C' }, ru: { normal: 'с', shift: 'С' } },
-  KeyV: { en: { normal: 'v', shift: 'V' }, ru: { normal: 'м', shift: 'М' } },
-  KeyB: { en: { normal: 'b', shift: 'B' }, ru: { normal: 'и', shift: 'И' } },
-  KeyN: { en: { normal: 'n', shift: 'N' }, ru: { normal: 'т', shift: 'Т' } },
-  KeyM: { en: { normal: 'm', shift: 'M' }, ru: { normal: 'ь', shift: 'Ь' } },
-  Comma: { en: { normal: ',', shift: '<' }, ru: { normal: 'б', shift: 'Б' } },
-  Period: { en: { normal: '.', shift: '>' }, ru: { normal: 'ю', shift: 'Ю' } },
-  Slash: { en: { normal: '/', shift: '?' }, ru: { normal: '.', shift: ',' } },
-  ArrowUp: '&#9650',
-  ShiftRight: 'Shift',
-  ControlLeft: 'Ctrl',
-  MetaLeft: 'Win',
-  AltLeft: 'Alt',
-  Space: 'Space',
-  AltRight: 'Alt',
-  ArrowLeft: '◀',
-  ArrowDown: '&#9660',
-  ArrowRight: '▶',
-  ControlRight: 'Ctrl',
-};
+
+  {
+    keyCode: 'Quote',
+    en: { normal: "'", shift: '"' },
+    ru: { normal: 'э', shift: 'Э' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'Enter',
+    en: { normal: 'Enter' },
+    ru: { normal: 'Enter' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'ShiftLeft',
+    en: { normal: 'Shift' },
+    ru: { normal: 'Shift' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'KeyZ',
+    en: { normal: 'z', shift: 'Z' },
+    ru: { normal: 'я', shift: 'Я' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyX',
+    en: { normal: 'x', shift: 'X' },
+    ru: { normal: 'ч', shift: 'Ч' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyC',
+    en: { normal: 'c', shift: 'C' },
+    ru: { normal: 'с', shift: 'С' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyV',
+    en: { normal: 'v', shift: 'V' },
+    ru: { normal: 'м', shift: 'М' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyB',
+    en: { normal: 'b', shift: 'B' },
+    ru: { normal: 'и', shift: 'И' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyN',
+    en: { normal: 'n', shift: 'N' },
+    ru: { normal: 'т', shift: 'Т' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'KeyM',
+    en: { normal: 'm', shift: 'M' },
+    ru: { normal: 'ь', shift: 'Ь' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'Comma',
+    en: { normal: ',', shift: '<' },
+    ru: { normal: 'б', shift: 'Б' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'Period',
+    en: { normal: '.', shift: '>' },
+    ru: { normal: 'ю', shift: 'Ю' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'Slash',
+    en: { normal: '/', shift: '?' },
+    ru: { normal: '.', shift: ',' },
+    universalButton: false,
+  },
+
+  {
+    keyCode: 'ArrowUp',
+    en: { normal: '↑' },
+    ru: { normal: '↑' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'ShiftRight',
+    en: { normal: 'Shift' },
+    ru: { normal: 'Shift' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'ControlLeft',
+    en: { normal: 'Ctrl' },
+    ru: { normal: 'Ctrl' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'MetaLeft',
+    en: { normal: 'Win' },
+    ru: { normal: 'Win' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'AltLeft',
+    en: { normal: 'Alt' },
+    ru: { normal: 'Alt' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'Space',
+    en: { normal: 'Space' },
+    ru: { normal: 'Space' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'AltRight',
+    en: { normal: 'Alt' },
+    ru: { normal: 'Alt' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'ArrowLeft',
+    en: { normal: '←' },
+    ru: { normal: '←' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'ArrowDown',
+    en: { normal: '↓' },
+    ru: { normal: '↓' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'ArrowRight',
+    en: { normal: '→' },
+    ru: { normal: '→' },
+    universalButton: true,
+  },
+  {
+    keyCode: 'ControlRight',
+    en: { normal: 'Ctrl' },
+    ru: { normal: 'Ctrl' },
+    universalButton: true,
+  },
+];
